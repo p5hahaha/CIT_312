1196e094a80219b4ec195273e2047898e6df6090
Jason <coolmangreg11@gmail.com>
2013-05-20 11:29:25 -0600

    Beginning Class Monday

---
+++ Java Source/Test Folder/test/Test_Multi_Thread.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Test_Multi_Thread {
+	private static final ExecutorService threadPool = Executors.newCachedThreadPool();
+	
+	while(true){
+		final Socket clientConnection = serverSocket.accept();
+		threadPool.execute(new Runnable(){
+			public void run(){
+				processConnection(clientConnection);
+			}
+		});
+	}
+
+}
