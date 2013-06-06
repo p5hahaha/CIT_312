93e2240aef30e2a2a12889a3aa6e801b774bf29a
Jason <coolmangreg11@gmail.com>
2013-06-06 11:32:47 -0600

    Update

--- Java Source/Test Folder/test/HQL_Test/User.java
+++ Java Source/Test Folder/test/HQL_Test/User.java
@@ -1,5 +1,37 @@
 package test.HQL_Test;
 
+@Entity
+@Table(name = "USER_TABLE")
 public class User {
-
+	
+	@Id
+	@GeneratedValue
+	private int user_id;
+	private String username;
+	private String password;
+	
+	User(int user_id, String username, String password){
+		this.user_id = user_id;
+		this.username = username;
+		this.password = password;
+	}
+	
+	public int getId(){
+		return user_id;
+	}
+	public String getUsername(){
+		return username;
+	}
+	public String getPassword(){
+		return password;
+	}
+	public void setId(int user_id){
+		this.user_id = user_id;
+	}
+	public void setUsername(String username){
+		this.username = username;
+	}
+	public void setPassword(String password){
+		this.password = password;
+	}
 }
