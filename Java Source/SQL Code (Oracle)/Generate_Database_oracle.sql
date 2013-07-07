d352f562243673cc6c76bb3e0575b867f9e7d276
Jason <coolmangreg11@gmail.com>
2013-07-06 19:16:01 -0600

    Diagramming

--- Java Source/SQL Code (Oracle)/Generate_Database_oracle.sql
+++ Java Source/SQL Code (Oracle)/Generate_Database_oracle.sql
@@ -62,3 +62,42 @@ GRANT SELECT, INSERT, UPDATE ON student.user_session to application;
 
 CREATE SEQUENCE s1_user_session;
 GRANT SELECT on student.s1_user_session to application;
+-- ------------------Vehicle Table-------------------------------------
+BEGIN
+  FOR i IN (SELECT   object_name, object_type
+            FROM     user_objects
+            WHERE    object_name IN ('VEHICLE', 'S1_VEHICLE')) LOOP
+    IF i.object_type = 'TABLE' THEN       
+      EXECUTE IMMEDIATE 'DROP TABLE '||i.object_name||' CASCADE CONSTRAINTS';
+    ELSIF i.object_type = 'SEQUENCE' THEN
+      EXECUTE IMMEDIATE 'DROP SEQUENCE '||i.object_name;
+    ELSE
+      dbms_output.put_line('Unknown object type: ' || i.object_type);
+    END IF;  
+  END LOOP;
+END;
+/
+
+CREATE TABLE vehicle
+( vehicle_id			INT PRIMARY KEY
+, vehicle_name			VARCHAR2(30)
+, user_id				INT
+, vehicle_year			INT
+, vehicle_avg_mpg		NUMBER
+, vehicle_gas_tank_size	NUMBER
+, current_gas_amount	NUMBER
+, current_odometer		NUMBER
+, vehicle				VARCHAR2(30)
+, created_by			INT
+, creation_date			DATE
+, last_updated_by		INT
+, last_update_date		DATE
+, ended_by				INT
+, ended_date			DATE
+);
+
+GRANT SELECT, INSERT, UPDATE ON student.vehicle to application;
+
+CREATE SEQUENCE s1_vehicle;
+GRANT SELECT on student.s1_vehicle to application;
+
